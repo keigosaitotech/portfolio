432773ec3ecb8bb2ead05d82ddeef0f9f191d078
keigosaitou <148031808+keigosaitou@users.noreply.github.com>
2024-03-27 16:01:45 +0900

    齊藤

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrap();
+        
 
         if (\App::environment(['production'])) {
             \URL::forseScheme('https');
         }
+
+        Paginator::useBootstrap();
     }
 }
